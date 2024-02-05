fbbef6252421480e75f14399f4ba2598ffdecbcc
Pythonesa <pythonesamar@gmail.com>
2024-02-04 22:24:14 -0300

    :sparkles: Calcular punto medio en espacio bidimensional

---
+++ basicos/soluciones/003.py
@@ -0,0 +1,6 @@
+ax = float(input("Ingresa la coordenada x del primer punto: "))
+ay = float(input("Ingresa la coordenada y del primer punto: "))
+bx = float(input("Ingresa la coordenada x del segundo punto: "))
+by = float(input("Ingresa la coordenada y del segundo punto: "))
+
+print("El punto medio es: ", (ax + bx) / 2, (ay + by) / 2)
